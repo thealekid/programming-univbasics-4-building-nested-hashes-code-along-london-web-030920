bf1706550c8138e7b3f5d70a409aa8d5d11f9291
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-07 17:15:58 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -14,8 +14,8 @@ def bonus
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
-          {name: "Steven", age: "30", attitude: "confused"}
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
