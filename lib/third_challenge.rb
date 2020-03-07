486c64a4b3e6d52a0bd0aab29013b2cf6d446453
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-07 16:48:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,13 +8,9 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: 
         
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
         },
       :heroine_friends => []
    }
